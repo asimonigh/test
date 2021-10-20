2e26a4141970ab113c19bbaa690ea542d915e772
anthony <anthony.simonigh.pro@gmail.com>
2021-10-20 15:47:30 +0200

    - add dependencies - init applicatiob

---
+++ mojo/app/src/main/java/com/simonigh/mojo/MojoTestApplication.kt
@@ -0,0 +1,27 @@
+package com.simonigh.mojo
+
+import android.app.Application
+import com.simonigh.mojo.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+class MojoTestApplication : Application() {
+    
+    override fun onCreate() {
+        super.onCreate()
+    
+        Timber.plant(DebugTree())
+    
+        startKoin {
+            androidLogger()
+            androidContext(this@MojoTestApplication)
+            modules(appModule)
+        }
+        
+    }
+    
+    
+}
---
+++ mojo/app/src/main/java/com/simonigh/mojo/di/AppModule.kt
@@ -0,0 +1,7 @@
+package com.simonigh.mojo.di
+
+import org.koin.dsl.module
+
+val appModule = module {
+
+}
